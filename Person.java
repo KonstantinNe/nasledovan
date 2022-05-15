3b1cdf344a28a8dbfadcd2ac7226a04c6154b3ff
Konstantin <nemeshkv@gmail.com>
2022-05-15 15:16:22 +0500

    Initial commit

---
+++ Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    int firstName;
+    int lastName;
+    int age;
+}
+public class Pilot extends Person{
+    int pilotclass;
+    Pilot pilot1 = new Pilot(firstName "Vasia", lastName "Vasin",age 30,pilotclass 1);
+    Pilot pilot2 = new Pilot(firstName "Petya", lastName "Petin",age 32),pilotclass 2;
+
+}
